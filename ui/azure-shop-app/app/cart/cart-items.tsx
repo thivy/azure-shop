7fa647cdea4e46a3cf50852d0458ecbab077a024
thivy <thivyakanth@gmail.com>
2023-01-09 10:54:55 +1100

    adding animation to cart

--- ui/azure-shop-app/app/cart/cart-items.tsx
+++ ui/azure-shop-app/app/cart/cart-items.tsx
@@ -3,16 +3,35 @@
 import { Panel } from "@/components/panel";
 import { useShopContext } from "app/shop/shop-context";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { placeOrder } from "./services/order-service";
 
 export const CartItems = () => {
   const context = useShopContext();
   const router = useRouter();
+  const [isFetching, setIsFetching] = useState(false);
 
   const placeAndCompleteOrder = async () => {
-    await placeOrder(context.cart);
-    context.clearCart();
-    router.push("/");
+    if (isFetching) return;
+
+    try {
+      setIsFetching(true);
+      await placeOrder(context.cart);
+      context.clearCart();
+      router.push("/");
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsFetching(false);
+    }
+  };
+
+  const totalCost = (price: number, quantity: number) => {
+    // rounde to 2 decimal places and multiply by quantity to get total cost
+    const p = price * quantity;
+
+    const returnPrice = Math.round(p * 100) / 100;
+    return returnPrice;
   };
 
   return (
@@ -24,12 +43,14 @@ export const CartItems = () => {
         >
           <div className="col-span-4">{item.prodct.name}</div>
           <div className="">{item.quantity}</div>
-          <div className="">${item.quantity * item.prodct.price}</div>
+          <div className="">${totalCost(item.prodct.price, item.quantity)}</div>
         </Panel>
       ))}
       <div className="py-4">
         <button
-          className="bg-blue-500 text-white px-4 py-2 rounded"
+          className={`bg-blue-500 text-white px-4 py-2 rounded ${
+            isFetching ? "animate-pulse" : ""
+          }`}
           onClick={() => placeAndCompleteOrder()}
         >
           Place Order
