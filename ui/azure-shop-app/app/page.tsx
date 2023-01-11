07402d0b98ead83e931ee173f526e516c95cf1ac
thivy <thivyakanth@gmail.com>
2023-01-11 11:04:02 +1100

    update logo

--- ui/azure-shop-app/app/page.tsx
+++ ui/azure-shop-app/app/page.tsx
@@ -24,10 +24,10 @@ export default function Home() {
         <h3 className="display-3">Build modular applications</h3>
       </Section>
       <Section>
-        <div className="grid lg:grid-cols-3 grid-flow-row gap-6 md:grid-cols-2 sm:grid-cols-1">
-          <Panel className="" />
-          <Panel className="" />
-          <Panel className="" />
+        <div className="grid lg:grid-cols-1 grid-flow-row gap-6 md:grid-cols-2 sm:grid-cols-1">
+          <Panel className="">
+            <img src="./high-level-architecture.svg" />
+          </Panel>
         </div>
       </Section>
     </>
