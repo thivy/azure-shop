eec7d4856a4a1639e0cb8adb8640e4a935024815
thivy <thivyakanth@gmail.com>
2023-03-13 08:39:40 +1100

    remove Suspense

--- ui/azure-shop-app/app/shop/[id]/page.tsx
+++ ui/azure-shop-app/app/shop/[id]/page.tsx
@@ -1,8 +1,6 @@
 import { uiDebug } from "@features/settings";
 import Pagination from "@features/shop/Pagination";
 import { ProudctResults } from "@features/shop/product-results";
-import { ProudctsLoading } from "@features/shop/products-loading";
-import { Suspense } from "react";
 
 export const revalidate = 0;
 
@@ -11,12 +9,11 @@ export default function Shop({ params }: any) {
   //preload();
   return (
     <div className={`my-4 ${uiDebug(false)}`}>
-      <Suspense fallback={<ProudctsLoading />}>
-        {/* @ts-expect-error Server Component */}
-        <Pagination activePage={params.id} />
-        {/* @ts-expect-error Server Component */}
-        <ProudctResults id={params.id} />
-      </Suspense>
+      {/* TODO: demo add suspense */}
+      {/* @ts-expect-error Server Component */}
+      <Pagination activePage={params.id} />
+      {/* @ts-expect-error Server Component */}
+      <ProudctResults id={params.id} />
     </div>
   );
 }
--- ui/azure-shop-app/features/shop/product-result-item.tsx
+++ ui/azure-shop-app/features/shop/product-result-item.tsx
@@ -1,9 +1,9 @@
 import { Panel } from "@components/panel";
 import { uiDebug } from "@features/settings";
 import Image from "next/image";
-import { FC, Suspense } from "react";
+import { FC } from "react";
 import { ProductAdd } from "./product-add";
-import { ProudctVote, ProudctVoteLoading } from "./product-vote";
+import { ProudctVote } from "./product-vote";
 import { IProduct } from "./services/models";
 
 interface IProp {
@@ -33,10 +33,9 @@ export const ProudctResultItem: FC<IProp> = (props) => {
         <div>${props.product.price}</div>
         <ProductAdd product={_prop.product} />
       </div>
-      <Suspense fallback={<ProudctVoteLoading />}>
-        {/* @ts-expect-error Server Component */}
-        <ProudctVote />
-      </Suspense>
+      {/* TODO: demo add suspense */}
+      {/* @ts-expect-error Server Component */}
+      <ProudctVote />
     </Panel>
   );
 };
