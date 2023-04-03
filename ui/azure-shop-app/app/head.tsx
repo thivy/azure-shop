5e9438a4aa12308a7da9ff6afbf8b3f9d6af2e63
thivy <thivyakanth@gmail.com>
2023-04-04 08:51:26 +1000

    add g script

--- ui/azure-shop-app/app/head.tsx
+++ ui/azure-shop-app/app/head.tsx
@@ -7,36 +7,20 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Azure Shop" />
       <link rel="icon" href="/favicon.ico" />
-      <Script
-        id="clarity"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "giij121s5g");`,
-        }}
-      />
+
       <Script
         id="google-analytics"
         strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-NL05REEWCG"
       />
-      <Script
-        id="google-analytics-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag("js", new Date());
-gtag("config", "G-NL05REEWCG");`,
-        }}
-      />
+      <Script id="google-analytics-script" strategy="afterInteractive">
+        {`
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+       
+         gtag('config', 'G-NL05REEWCG');`}
+      </Script>
     </>
   );
 }
