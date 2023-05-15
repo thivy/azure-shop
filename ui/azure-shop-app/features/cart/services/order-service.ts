d0df973425cf6635770fce7e474994bcad1088c0
thivy <thivyakanth@gmail.com>
2023-05-16 09:39:56 +1000

    fix order API

--- ui/azure-shop-app/features/cart/services/order-service.ts
+++ ui/azure-shop-app/features/cart/services/order-service.ts
@@ -1,11 +1,38 @@
+"use server";
+import { CommunicationProtocolEnum, DaprClient } from "@dapr/dapr";
+import { fetcher } from "@features/core/fetch/app-fetch";
+import { randomUUID } from "crypto";
 import { IOrder } from "./models";
 
-export const placeOrder = async (order: IOrder) => {
-  await fetch("/api/order", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(order),
+const daprHost = "127.0.0.1";
+
+const addToTopic = async (orderId: string) => {
+  const client = new DaprClient({
+    communicationProtocol: CommunicationProtocolEnum.HTTP, // default
+    daprHost: daprHost, // default
+    daprPort: process.env.DAPR_HTTP_PORT, // default
   });
+  await client.pubsub.publish(
+    process.env.PUB_SUB_NAME,
+    process.env.PUB_SUB_TOPIC,
+    orderId
+  );
+};
+
+export const placeOrder = async (order: IOrder): Promise<void> => {
+  const uuid = randomUUID();
+  order.cart.forEach(async (item) => {
+    const _order = {
+      orderId: uuid,
+      productid: item.product.id,
+      quantity: item.quantity,
+      productname: item.product.name,
+    };
+    const api = `${process.env.CMS_API}/api/collections/orders/records`;
+    await fetcher(api, {
+      method: "POST",
+      body: JSON.stringify(_order),
+    });
+  });
+  addToTopic(uuid);
 };
--- ui/azure-shop-app/type.ts
+++ ui/azure-shop-app/type.ts
@@ -1,6 +1,11 @@
-const requiredServerEnvs = ["CMS_API", "IMAGE_API"] as const;
+const requiredServerEnvs = [
+  "CMS_API",
+  "IMAGE_API",
+  "PUB_SUB_NAME",
+  "PUB_SUB_TOPIC",
+] as const;
 
-type RequiredServerEnvKeys = typeof requiredServerEnvs[number];
+type RequiredServerEnvKeys = (typeof requiredServerEnvs)[number];
 
 declare global {
   namespace NodeJS {
